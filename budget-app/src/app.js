bea307e1590231eacc3ef17de4f1bd70715f2258
Anthony Sim <anthonyysim@gmail.com>
2021-01-30 22:22:06 -0800

    added title of project

--- budget-app/src/app.js
+++ budget-app/src/app.js
@@ -1,11 +1,12 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const App = () => (
   <div>
-    <h1>hello</h1>
-
-  </div>
+    <h1 className="display-4 mt-4 text-center text-info">Budget Expense Tracker</h1>
+  </div >
 );
 
 export default App;
--- budget-app/src/index.js
+++ budget-app/src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+// App Component
 import App from "./App";
+
+// Apply CSS to React App
+import './app.css'
 
 ReactDOM.render(<App />, document.querySelector("#root"));
