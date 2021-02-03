b37eee903ce09da9c1a662542e4d7a7b14931a56
Anthony Sim <anthonyysim@gmail.com>
2021-02-02 21:27:36 -0800

    downloaded react icons, added update & delete buttons

--- budget-app/src/components/AllTransactions.js
+++ budget-app/src/components/AllTransactions.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Table } from 'react-bootstrap';
+import { Table, Button } from 'react-bootstrap';
+import { FaTrashAlt } from 'react-icons/fa';
+import { MdUpdate } from 'react-icons/Md';
 
 
 const AllTransactions = ({ items }) => {
   // console.log(items)
   let transactions = items.map(({ payee, type, memo, amount }) => {
     if (type === 'Expense') {
-      return <tr><th>{payee}</th><th>{type}</th><th>{memo}</th><th>${amount.toFixed(2)}</th><th></th></tr>
+      return <tr><th>{payee}</th><th>{type}</th><th>{memo}</th><th>${amount.toFixed(2)}</th><th></th><th><Button variant="outline-info" size="sm">Update</Button></th>
+        <th><Button variant="outline-danger" size="sm">Delete</Button></th></tr>
 
     } else {
-      return <tr><th>{payee}</th><th>{type}</th><th>{memo}</th><th></th><th>${amount.toFixed(2)}</th></tr>
+      return <tr><th>{payee}</th><th>{type}</th><th>{memo}</th><th></th><th>${amount.toFixed(2)}</th><th><Button variant="outline-info" size="sm">Update</Button></th>
+        <th><Button variant="outline-danger" size="sm">Delete</Button></th></tr>
     }
   })
 
@@ -23,17 +27,21 @@ const AllTransactions = ({ items }) => {
             <th>Memo</th>
             <th>Expense</th>
             <th>Income</th>
+            <th><MdUpdate className="text-info" /></th>
+            <th><FaTrashAlt className="text-danger" /></th>
           </tr>
         </thead>
         <tbody>
           {transactions}
-          {/* <tr>
+          <tr>
             <td>Landlord</td>
             <td>Expense</td>
             <th>Landlord wants 50% of paycheck.... wt 🦆?</th>
             <th>$1600.00</th>
             <th></th>
-          </tr> */}
+            <th><Button variant="outline-info" size="sm">Update</Button></th>
+            <th><Button variant="outline-danger" size="sm">Delete</Button></th>
+          </tr>
         </tbody>
       </Table>
     </div >
