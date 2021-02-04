d3fb9fd8b93a41ed2ac8ba5a1c5cdc36a5078476
Anthony Sim <anthonyysim@gmail.com>
2021-02-04 00:00:00 -0800

    removed source mapper that was causing loading issues

--- budget-app/webpack.config.js
+++ budget-app/webpack.config.js
@@ -35,11 +35,6 @@ module.exports = {
         test: /\.png|svg|jpg|gif$/,
         use: ["file-loader"],
       },
-      {
-        test: /\.js$/,
-        enforce: 'pre',
-        use: ['source-map-loader'],
-      },
     ],
   },
 };
